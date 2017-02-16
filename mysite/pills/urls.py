30706f7b65c3af2687842715865d29455dbfdd41
chang xu <xcltw1993@gmail.com>
2017-02-16 21:27:25 +0800

    # t3

--- mysite/pills/urls.py
+++ mysite/pills/urls.py
@@ -2,6 +2,10 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'pills'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^P<question_id>[0-9]+', views.detail, name='detail'),
+    url(r'^P<question_id>[0-9]+/result', views.result, name='result'),
+    url(r'^P<question_id>[0-9]+/vote', views.vote, name='vote'),
 ]
