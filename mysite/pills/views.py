84eaafc893f9aacd932031cf96e7f5e6e7458a93
xuchang <chang.xu@speedx.com>
2017-02-24 17:52:17 +0800

    push in company

--- mysite/pills/views.py
+++ mysite/pills/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello,world. It's my pills index!")
+    last_question_list = Question.objects.order_by('-pub_date')[:3]
+    context = {'latest_question_list': last_question_list}
+    return render(request, 'pills/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'pills/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'pills/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'pills/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('pills:results', args=(question_id,)))
