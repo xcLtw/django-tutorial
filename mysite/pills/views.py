adc86b3b88a95d9814344cee36bd97c55495102b
chang xu <xcltw1993@gmail.com>
2017-03-10 00:05:54 +0800

    use generic template,t4,home

--- mysite/pills/views.py
+++ mysite/pills/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Question, Choice
+from django.views import generic
 
 
 # Create your views here.
-def index(request):
-    last_question_list = Question.objects.order_by('-pub_date')[:3]
-    context = {'latest_question_list': last_question_list}
-    print('^^^==', context)
-    return render(request, 'pills/index.html', context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'pills/detail.html', {'question': question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'pills/results.html', {'question': question})
+# def index(request):
+#     last_question_list = Question.objects.order_by('-pub_date')[:3]
+#     context = {'latest_question_list': last_question_list}
+#     print('^^^==', context)
+#     return render(request, 'pills/index.html', context)
+#
+#
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'pills/detail.html', {'question': question})
+#
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'pills/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -35,3 +36,21 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('pills:results', args=(question_id,)))
+
+
+class IndexView(generic.ListView):
+    template_name = 'pills/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'pills/detail.html'
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'pills/results.html'
